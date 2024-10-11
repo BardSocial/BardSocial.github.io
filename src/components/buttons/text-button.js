60a7e83df2aa826e451528b0d9890265ec1857a4
Joe Czepil <joe.czepil@getsmartsolutions.ca>
2024-10-11 10:36:19 -0700

    push public folder

--- src/components/buttons/text-button.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const TextButton = styled.button`
-  color: ${({ theme }) => theme.colors.bodyText};
-  font-weight: 500;
-  text-transform: ${({ upper }) => (upper ? 'uppercase' : 'auto')};
-`;
-
-export default TextButton;
